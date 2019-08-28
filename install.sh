c367e5f5cbbddf982f551eca1180c055c484dbbe
chiphwang <chip.hwang@gmail.com>
2019-08-28 15:02:49 -0700

    added +r to .kube/config

--- install.sh
+++ install.sh
@@ -121,6 +121,7 @@ sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+sudo chmod +r $HOME/.kube/config
 #Apply Flannel CNI network overlay:
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
 #Verify the kubernetes node:
@@ -157,6 +158,8 @@ kubectl create clusterrolebinding tiller \
 
 helm init --service-account tiller
 
+# Move to working directory
 
+cd /opt/fossa_helm/
 
 
